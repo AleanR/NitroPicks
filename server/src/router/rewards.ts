df3c640844935c7bde76ae36b71e5c1a0d020ca4
Jase Thomas <jase.thomas04@gmail.com>
2026-04-05 00:26:04 -0400

    Added rewards.ts under routers
    Added post method for redeeming reward

---
+++ server/src/router/rewards.ts
@@ -0,0 +1,7 @@
+import { Router } from 'express';
+import { redeemReward } from '../controllers/rewards';
+import { isAuthenticated } from '../middlewares';
+
+export default (router: Router) => {
+    router.post('users/:id/redeem', isAuthenticated, redeemReward);
+};
