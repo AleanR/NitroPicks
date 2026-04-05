aa33a9c43dc340d6d037fc7ebc966f7932d606a3
Jase Thomas <jase.thomas04@gmail.com>
2026-04-05 00:26:56 -0400

    Updated index.ts under routers
    Added routing for rewards.ts

--- server/src/router/index.ts
+++ server/src/router/index.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import authentication from './authentication';
 import users from './users';
 import games from './games';
+import rewards from './rewards';
 
 const router = Router();
 
@@ -9,5 +10,6 @@ export default (): Router => {
     authentication(router);
     users(router);
     games(router);
+    rewards(router);
     return router;
 }
