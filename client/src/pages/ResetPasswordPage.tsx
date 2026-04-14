795932f940f3fad7b14601af755ce449dc12cb2f
Alean Rodriguez <rodriguezalean005@gmail.com>
2026-04-13 20:27:30 -0400

    Fix Reset Password page.

--- client/src/pages/ResetPasswordPage.tsx
+++ client/src/pages/ResetPasswordPage.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import { useParams, useNavigate, Link } from 'react-router-dom'
 
 type PageState = 'form' | 'success' | 'expired'
